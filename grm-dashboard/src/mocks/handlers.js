bad110dab8600148592204770a9483929e651e96
Bryce Rothhammer <rothhammer.b@gmail.com>
2025-01-23 13:56:47 -0700

    set up msw handler to fail 25% of the time and have a 2 second delay to show loading state

--- grm-dashboard/src/mocks/handlers.js
+++ grm-dashboard/src/mocks/handlers.js
@@ -1,8 +1,18 @@
-import { http, HttpResponse } from 'msw'
+import { http, HttpResponse, delay } from 'msw'
 import mockData from './mockData'
 
 export const handlers = [
-  http.get('/data.json', () => {
+  http.get('/data.json', async () => {
+    await delay(2000) // 2 second delay
+
+    // Randomly fail 25% of the time
+    if (Math.random() > 0.75) {
+      return new HttpResponse(null, {
+        status: 500,
+        statusText: 'Internal Server Error'
+      })
+    }
+
     return HttpResponse.json(mockData)
   })
 ]
